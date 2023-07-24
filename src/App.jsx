f68cc5710a7e1d3ab98011013d86372dbd84c745
Antonio Ricardo <antoniorcs67@gmail.com>
2023-07-24 02:47:07 -0300

    start of project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <header className='main-header'>HEADER</header>
+      <div className='main-about'>ABOUT</div>
+      <div className='main-skills'>SKILLS</div>
+      <div className='main-experiences'>EXPERIENCES</div>
+      <div className='main-contact'>CONTACT</div>
+    </React.Fragment>
+  )
+}
+
+export default App
